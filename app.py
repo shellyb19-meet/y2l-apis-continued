f42f65605e02ae69347e6bffc6fabc0a60c13bed
aboud20-meet <aboud20@meet.mit.edu>
2018-12-13 18:48:27 +0200

    i did itgit add .!

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
+import requests, json
 app = Flask(__name__)
+
+
 
 @app.route('/')
 def home():
@@ -15,8 +18,12 @@ def study_image():
     # example we covered in the slides! 
 
     # YOUR CODE HERE!
+    headers = {'Authorization': 'Key f2f339a3cc374420a221fa27e58a3202'}
+    api_url = "https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/outputs"
+    data ={"inputs": [{"data": {"image": {"url": image_url}}}]}
+    response = requests.post(api_url, headers=headers, data=json.dumps(data))
     
-    return render_template('home.html', results="No results yet :(")
+    return render_template('home.html', results=response.content)
 
 if __name__ == '__main__':
     app.run(debug=True)
